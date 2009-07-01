bb08e4f09ce67f6cc53f08d47ae8582ba279cacc
Leonard Dahlmann <leo.dahlmann@gmail.com>
2009-07-01 21:45:40 +0000

    ID -> Id

--- Resource.hs
+++ Resource.hs
@@ -1,4 +1,4 @@
-module Resource (Renderer, image, ImageMap, ResID, runRenderer, newImageMap, drawImage) where
+module Resource (Renderer, image, ImageMap, ResId, runRenderer, newImageMap, drawImage) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -7,8 +7,8 @@ import Graphics.UI.SDL
 
 import Util
 
-type ResID = String
-type ResMap a = Map ResID a
+type ResId = String
+type ResMap a = Map ResId a
 
 type ImageMap = ResMap Surface
 
@@ -17,7 +17,7 @@ type Renderer a = StateT ImageMap IO a
 newImageMap :: ImageMap
 newImageMap = Map.empty
 
-image :: ResID -> Renderer Surface
+image :: ResId -> Renderer Surface
 image name = do res <- get
                 case name `Map.lookup` res of
                     (Just a) -> return a
@@ -25,7 +25,7 @@ image name = do res <- get
                                   put (Map.insert name surface res)
                                   return surface
 
-drawImage :: ResID -> Point2 -> Surface ->  Renderer ()
+drawImage :: ResId -> Point2 -> Surface ->  Renderer ()
 drawImage name (x, y) sur = do img <- image name
                                b <- liftIO $ blitSurface img Nothing sur (Just $ Rect x y 0 0)
                                return ()
