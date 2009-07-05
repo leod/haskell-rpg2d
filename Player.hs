83f1a1619dbc1abc9d82e115f20d4d68ec3025a6
Leonard Dahlmann <leo.dahlmann@gmail.com>
2009-07-05 22:36:02 +0000

    factored out animation

--- Player.hs
+++ Player.hs
@@ -8,13 +8,13 @@ import Util
 import Input
 import Render
 import Consts
+import Anim
 
 data Player = Player { pos :: Point2
                      , vel :: Point2
                      , dir :: Direction
-
-                     , frame :: Int
-                     , frameCtr :: Int
+                     , anim :: Anim
+                     , walking :: Bool
                      } deriving Show
 
 clip :: Point2
@@ -24,8 +24,6 @@ dirToRow DirLeft = 0
 dirToRow DirUp = 1
 dirToRow DirRight = 2
 dirToRow DirDown = 3
-
-animTime = 15
 
 file = "linkanim.png"
 
@@ -39,34 +37,29 @@ instance Actor Player where
             vel' = fromMaybe (0, 0) $ dirToVel `liftM` dir'
             pos' = pos self ^+ vel'
 
-            walking = vel' /= (0, 0)
+            walking' = vel' /= (0, 0)
 
-            frameCtr' = if walking then (if frameCtr self == animTime then 0 else frameCtr self + 1) else frameCtr self
-
-            frame' = if walking then
-                         if frameCtr' == animTime then
-                             if frame self == 7 then 0
-                             else frame self + 1
-                         else frame self
-                     else 0
+            anim' = if walking' then updateAnim 13 7 $ anim self else fixFrame 0
 
         evMoveCamera (-px pos' + viewWidth `div` 2 - px clip `div` 2,
                       -py pos' + viewHeight `div` 2 - py clip `div` 2)
 
         return self { pos = pos'
                     , dir = fromMaybe (dir self) dir'
-                    , frameCtr = frameCtr'
-                    , frame = frame'
+                    , walking = walking'
+                    , anim = anim'
                     }
         
     posRect self = Rect 0 0 0 0
-    render self sprs = spriteClipped (getSprite file sprs) (pos self) (px clip * frame self, py clip * dirToRow (dir self)) clip
+    render self sprs = spriteClipped (getSprite file sprs)
+                                     (pos self)
+                                     (px clip * (animFrame . anim) self, py clip * dirToRow (dir self)) clip
 
 newPlayer :: Point2 -> AnyActor
 newPlayer p = AnyActor $
     Player { pos = p
            , vel = (0, 0)
            , dir = DirLeft
-           , frame = 0
-           , frameCtr = 0
+           , walking = False
+           , anim = fixFrame 0 
            }
