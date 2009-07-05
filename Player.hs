76ca63045167160b313937f22693809ebc9e3a07
Leonard Dahlmann <leo.dahlmann@gmail.com>
2009-07-05 19:43:57 +0000

    hackish walk animation

--- Player.hs
+++ Player.hs
@@ -12,15 +12,20 @@ import Consts
 data Player = Player { pos :: Point2
                      , vel :: Point2
                      , dir :: Direction
+
+                     , frame :: Int
+                     , frameCtr :: Int
                      } deriving Show
 
 clip :: Point2
-clip = (23, 25)
+clip = (23, 26)
 
 dirToRow DirLeft = 0
 dirToRow DirUp = 1
 dirToRow DirRight = 2
 dirToRow DirDown = 3
+
+animTime = 15
 
 file = "linkanim.png"
 
@@ -34,13 +39,33 @@ instance Actor Player where
             vel' = fromMaybe (0, 0) $ dirToVel `liftM` dir'
             pos' = pos self ^+ vel'
 
+            walking = vel' /= (0, 0)
+
+            frameCtr' = if walking then (if frameCtr self == animTime then 0 else frameCtr self + 1) else frameCtr self
+
+            frame' = if walking then
+                         if frameCtr' == animTime then
+                             if frame self == 7 then 0
+                             else frame self + 1
+                         else frame self
+                     else 0
+
         evMoveCamera (-px pos' + viewWidth `div` 2, -py pos' + viewHeight `div` 2)
 
         return self { pos = pos'
-                    , dir = fromMaybe (dir self) dir' }
+                    , dir = fromMaybe (dir self) dir'
+                    , frameCtr = frameCtr'
+                    , frame = frame'
+                    }
         
     posRect self = Rect 0 0 0 0
-    render self sprs = spriteClipped (getSprite file sprs) (pos self) (0, py clip * dirToRow (dir self)) clip
+    render self sprs = spriteClipped (getSprite file sprs) (pos self) (px clip * frame self, py clip * dirToRow (dir self)) clip
 
 newPlayer :: Point2 -> AnyActor
-newPlayer p = AnyActor $ Player { pos = p, vel = (0, 0), dir = DirLeft }
+newPlayer p = AnyActor $
+    Player { pos = p
+           , vel = (0, 0)
+           , dir = DirLeft
+           , frame = 0
+           , frameCtr = 0
+           }
