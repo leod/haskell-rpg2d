c84b0a52ed3526db1d68c6aa43c1380b22679323
leod <subtle.frustration@proton.me>
2009-07-05 20:47:58 +0000

    hackish walk animation

--- TileMap.hs
+++ TileMap.hs
@@ -11,26 +11,27 @@ type Tile = Int
 type TileMap = Array Point2 Tile
 
 renderTileMap :: TileMap -> SpriteMap -> IO ()
-renderTileMap tm sm = let tiles = assocs tm
-                          spr = "test2.png" `getSprite` sm
-                      in withTexture (sprTexture spr) $ GL.renderPrimitive GL.Quads $ do 
-                        mapM_ (\((x', y'), t) ->
-                               let (x, y) = ((fromIntegral x')*tileWidth, (fromIntegral y')*tileHeight) :: (Double, Double)
-                                   (cx, cy) = (tileWidth*0, tileHeight*5) :: (Double, Double)
-                                   (cw, ch) = (tileWidth, tileHeight) :: (Double, Double)
-                                   (tx, ty) = (sprWidthRatio spr * (cx / (sprWidth spr)),
-                                               sprHeightRatio spr * (cy / (sprHeight spr)))
-                                   (tw, th) = (sprWidthRatio spr * (cw / (sprWidth spr)),
-                                               sprHeightRatio spr * (ch / (sprHeight spr)))
-                               in do
-                                   GL.texCoord $ GL.TexCoord2 tx ty
-                                   GL.vertex   $ GL.Vertex2 x y
+renderTileMap tm sm =
+    let tiles = assocs tm
+        spr = "test2.png" `getSprite` sm
+    in withTexture (sprTexture spr) $ GL.renderPrimitive GL.Quads $ do 
+        forM_ tiles (\((x', y'), t) ->
+            let (x, y) = ((fromIntegral x')*tileWidth, (fromIntegral y')*tileHeight) :: (Double, Double)
+                (cx, cy) = (tileWidth*0, tileHeight*5) :: (Double, Double)
+                (cw, ch) = (tileWidth, tileHeight) :: (Double, Double)
+                (tx, ty) = (sprWidthRatio spr * (cx / (sprWidth spr)),
+                            sprHeightRatio spr * (cy / (sprHeight spr)))
+                (tw, th) = (sprWidthRatio spr * (cw / (sprWidth spr)),
+                            sprHeightRatio spr * (ch / (sprHeight spr)))
+            in do
+                GL.texCoord $ GL.TexCoord2 tx ty
+                GL.vertex   $ GL.Vertex2 x y
+ 
+                GL.texCoord $ GL.TexCoord2 (tw+tx) ty
+                GL.vertex   $ GL.Vertex2 (x+cw) y
 
-                                   GL.texCoord $ GL.TexCoord2 (tw+tx) ty
-                                   GL.vertex   $ GL.Vertex2 (x+cw) y
+                GL.texCoord $ GL.TexCoord2 (tw+tx) (th+ty)
+                GL.vertex   $ GL.Vertex2 (x+cw) (y+ch)
 
-                                   GL.texCoord $ GL.TexCoord2 (tw+tx) (th+ty)
-                                   GL.vertex   $ GL.Vertex2 (x+cw) (y+ch)
-
-                                   GL.texCoord $ GL.TexCoord2 tx (th+ty)
-                                   GL.vertex   $ GL.Vertex2 x (y+ch)) tiles
+                GL.texCoord $ GL.TexCoord2 tx (th+ty)
+                GL.vertex   $ GL.Vertex2 x (y+ch))
