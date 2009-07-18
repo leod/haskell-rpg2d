670963f440f20474f25c8319707f53433c2c9a4c
leod <subtle.frustration@proton.me>
2009-07-18 02:20:19 +0000

    experimenting with accessors

--- Enemy.hs
+++ Enemy.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Enemy (newEnemy) where
 
 import System.IO.Unsafe
@@ -5,18 +6,23 @@ import Control.Monad
 import Control.Monad.Random
 import Control.Monad.Reader
 import Debug.Trace
+import Control.Monad.State hiding (get)
+import Data.Accessor.Template
+import Data.Accessor.Monad.MTL.State
 
 import Actor
 import Util
 import Render
 import Anim
 
-data Enemy = Enemy { pos :: !Point2
-                   , dir :: !Direction
-                   , anim :: !Anim
-                   , extraVel :: !Point2
-                   , extraVelTime :: !Int
+data Enemy = Enemy { pos_ :: !Point2
+                   , dir_ :: !Direction
+                   , anim_ :: !Anim
+                   , extraVel_ :: !Point2
+                   , extraVelTime_ :: !Int
                    } deriving Show
+
+$(deriveAccessors ''Enemy)
 
 clip :: (Int, Int)
 clip = (37, 40)
@@ -26,38 +32,47 @@ dirToColumn DirLeft = 1
 dirToColumn DirUp = 2
 dirToColumn DirRight = 3
 
-vel = dirToVel . dir
+vel = dirToVel . dir_
 
 instance Actor Enemy where
     neededResources _ = ["enemy2.png"]
 
-    update self = do
-        let anim' = updateAnim 8 2 $ anim self
-            pos' = pos self ^+^ vel self ^+^ extraVel self 
-            dir' = if px (pos self) <= 0 && dir self == DirLeft then DirRight else dir self
+    -- Some experimentation with using State+Accessors
+    update = execStateT $ do
+        anim %: updateAnim 8 2
 
-        return $ self { pos = pos'
-                      , anim = anim'
-                      , extraVel = if extraVelTime self == 0 then (0, 0) else extraVel self
-                      , extraVelTime = if extraVelTime self > 0 then extraVelTime self - 1 else extraVelTime self
-                      , dir = dir'
-                      }
+        v <- liftM dirToVel $ get dir
+        ve <- get extraVel
+        pos %: (^+^ v) . (^+^ ve) 
+
+        pos' <- get pos
+        dir' <- get dir
+        when (px pos' <= 0 && dir' == DirLeft) $
+            dir %= DirRight
+
+        velTime <- get extraVelTime
+        if velTime == 0
+            then extraVel %= (0,0)
+            else extraVelTime %: subtract 1
 
     render sprs self = spriteClipped (getSprite "enemy2.png" sprs)
-                                     (pos self)
-                                     (px clip * dirToColumn (dir self), py clip * (animFrame . anim) self)
+                                     (pos_ self)
+                                     (px clip * dirToColumn (dir_ self), py clip * (animFrame . anim_) self)
                                      clip
     
-    posRect self = mkRect (pos self ^+ 7) (20, 20)
+    posRect self = mkRect (pos_ self ^+ 7) (20, 20)
 
     message (Impact dmg imvel) self = do
-        return self { extraVel = imvel ^* 3 ^+^ vel self, extraVelTime = 2, dir = (oppositeDir . oppositeDir . dirFromVec) imvel }
+        return self { extraVel_ = imvel ^* 3 ^+^ vel self
+                    , extraVelTime_ = 2
+                    , dir_ = (oppositeDir . oppositeDir . dirFromVec) imvel
+                    }
     message _ self = return self
 
 newEnemy :: Point2 -> AnyActor
-newEnemy p = AnyActor Enemy { pos = p
-                            , dir = DirDown
-                            , anim = fixFrame 0
-                            , extraVel = (0, 0)
-                            , extraVelTime = 0
+newEnemy p = AnyActor Enemy { pos_ = p
+                            , dir_ = DirDown
+                            , anim_ = fixFrame 0
+                            , extraVel_ = (0, 0)
+                            , extraVelTime_ = 0
                             }
