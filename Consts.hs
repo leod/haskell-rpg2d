7b5712466e180652299baf25e69e94a589775019
leod <subtle.frustration@proton.me>
2009-07-23 01:47:42 +0000

    action for setting tiles

--- Consts.hs
+++ Consts.hs
@@ -3,6 +3,7 @@ module Consts
     , tileHeight
     , viewWidth
     , viewHeight
+    , numLayers
     ) where
 
 tileWidth, tileHeight :: (Num a) => a
@@ -12,6 +13,9 @@ tileHeight = 32
 viewWidth, viewHeight :: Int
 viewWidth = 320
 viewHeight = 240
+
+numLayers :: Int
+numLayers = 4
 
 {-screenWidth = 640-}
 {-screenHeight = 480-}
